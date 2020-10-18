d572216ae20c47ebb510bb6f296ed0ae7a57f22f
sonykurian96 <67534990+sonykurian96@users.noreply.github.com>
2020-10-18 20:54:17 +0530

    Delete library.dart

--- flutter_app/lib/pages/library.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-class Library extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Library'));
-  }
-}
