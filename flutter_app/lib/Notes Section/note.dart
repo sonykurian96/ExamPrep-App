c5acd216c168e69078f05280ffd42d05d760a866
sonykurian96 <67534990+sonykurian96@users.noreply.github.com>
2021-01-29 16:36:35 +0530

    Update note.dart

--- flutter_app/lib/Notes Section/note.dart
+++ flutter_app/lib/Notes Section/note.dart
@@ -1,22 +1,74 @@
 import 'package:flutter/material.dart';
+import "NotesPage.dart";
 
-class Note extends StatelessWidget {
+class Note extends StatefulWidget {
+  @override
+  _NoteState createState() => _NoteState();
+}
+
+class _NoteState extends State<Note> {
+  List<String> list = [
+    'Arrays',
+    'Linked list',
+    'Stacks',
+    'Queues',
+    'Trees',
+    'Heaps',
+    'Graphs',
+    'Hash Tables'
+  ];
+
+  List<String> videoList = [
+    "https://www.thedshandbook.com/arrays/",
+    "https://www.thedshandbook.com/linked-lists/",
+    "https://www.thedshandbook.com/stacks/",
+    "https://www.thedshandbook.com/queues/",
+    "https://www.thedshandbook.com/trees/",
+    "https://www.thedshandbook.com/heaps/",
+    "https://www.thedshandbook.com/graphs/",
+    "https://www.thedshandbook.com/hash-tables/"
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
-          body: Column(
-            children: [
-              SizedBox(height: 70,),
-              Image.asset("assets/under_construction(image).jpg"),
-              SizedBox(height: 20,),
-              Text("Under Construction",
-                style: TextStyle(
-                  fontSize: 20,
-                ),)
-            ],
-          )
+          body: ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => NotesPage(),
+                            settings: RouteSettings(
+                              arguments: videoList[index],
+                            ),
+                          ));
+                    },
+                    child: Card(
+                      shadowColor: Colors.orangeAccent,
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: ListTile(
+                        title: Text(
+                          list[index],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        leading: Icon(
+                          Icons.book,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }),
         )
     );
   }
