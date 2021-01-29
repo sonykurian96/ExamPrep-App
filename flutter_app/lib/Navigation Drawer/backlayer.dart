2a21d11b6c3e9717bd07c24e2dbbd50124fa5f84
sonykurian96 <67534990+sonykurian96@users.noreply.github.com>
2021-01-29 16:35:07 +0530

    Update backlayer.dart

--- flutter_app/lib/Navigation Drawer/backlayer.dart
+++ flutter_app/lib/Navigation Drawer/backlayer.dart
@@ -4,33 +4,32 @@ class BackLayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            width: 420,
-            height: 220,
-            decoration: BoxDecoration(
-              image: DecorationImage(image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSwwecgGCxGNGu6twlp_-30zp-6wYWdwIAv2w&usqp=CAU"),
-                  fit: BoxFit.cover),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.orange[800],Colors.amber]
+          )
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 5,),
+            ListTile(
+              title: Text("Assessment",style: TextStyle(color: Colors.white)),
+              leading: Icon(Icons.assessment,color: Colors.white,),
             ),
-          ),
-          SizedBox(height: 5,),
-          ListTile(
-            title: Text("Assessment",style: TextStyle(color: Colors.white)),
-            leading: Icon(Icons.assessment,color: Colors.white,),
-          ),
-          Divider(color: Colors.white,),
-          ListTile(
-            title: Text("Settings",style: TextStyle(color: Colors.white)),
-            leading: Icon(Icons.settings,color: Colors.white,),
-          ),
-          Divider(color: Colors.white,),
-          ListTile(
-            title: Text("Dark Mode Disabled",style: TextStyle(color: Colors.white)),
-            leading: Icon(Icons.brightness_high,color: Colors.white,),
-          ),
-          Divider(color: Colors.white,),
-        ],
+            Divider(color: Colors.white,),
+            ListTile(
+              title: Text("Settings",style: TextStyle(color: Colors.white)),
+              leading: Icon(Icons.settings,color: Colors.white,),
+            ),
+            Divider(color: Colors.white,),
+            ListTile(
+              title: Text("Dark Mode Disabled",style: TextStyle(color: Colors.white)),
+              leading: Icon(Icons.brightness_high,color: Colors.white,),
+            ),
+            Divider(color: Colors.white,),
+          ],
+        ),
       ),
     );
 
