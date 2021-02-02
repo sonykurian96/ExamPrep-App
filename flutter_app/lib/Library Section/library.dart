5b1984fdf22be54f0ede635b33ab0799e82bfc19
sonykurian96 <67534990+sonykurian96@users.noreply.github.com>
2021-02-02 21:10:37 +0530

    Update library.dart

--- flutter_app/lib/Library Section/library.dart
+++ flutter_app/lib/Library Section/library.dart
@@ -1,3 +1,4 @@
+import 'package:clickable_list_wheel_view/clickable_list_wheel_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'VideoPage.dart';
@@ -29,46 +30,64 @@ class _VideoListState extends State<VideoList> {
     "https://youtu.be/zp6pBNbUB2U", // queue
     "https://youtu.be/YAdLFsTG70w" // tree
   ];
+
+  final _scrollController = FixedExtentScrollController();
+  static const double _itemHeight = 60;
+  static const int _itemCount = 8;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => VideoPage(),
-                        settings: RouteSettings(
-                          arguments: videoList[index],
-                        ),
-                      ));
-                },
-                child: Card(
-                  shadowColor: Colors.orangeAccent,
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
+        backgroundColor: Colors.white,
+        body: ClickableListWheelScrollView(
+            scrollController: _scrollController,
+            itemHeight: _itemHeight,
+            itemCount: _itemCount,
+            onItemTapCallback: (index) {
+              print("you tapped this $index");
+              Navigator.push(context,
+                MaterialPageRoute(
+                  builder: (context) => VideoPage(),
+                  settings: RouteSettings(
+                  arguments: videoList[index],
                   ),
-                  child: ListTile(
-                    title: Text(
-                      list[index],
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    leading: Icon(
-                      Icons.ondemand_video,
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
+                )
+              );
+            },
+            child: ListWheelScrollView.useDelegate(
+              itemExtent: _itemHeight,
+              controller: _scrollController,
+              physics: FixedExtentScrollPhysics(),
+              childDelegate: ListWheelChildBuilderDelegate(
+                builder: (context, index) => _child(index),
+                childCount: _itemCount,
               ),
-            );
-          }),
+            )
+        )
+    );
+  }
+
+  Widget _child(int index) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+      child: Card(
+        shadowColor: Colors.amber,
+        elevation: 20,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: ListTile(
+          title: Text(
+            list[index],
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          leading: Icon(
+            Icons.ondemand_video,
+            color: Colors.red,
+          ),
+        ),
+      ),
     );
   }
 }
+
